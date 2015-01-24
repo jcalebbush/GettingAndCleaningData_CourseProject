2adc94c44c2555264f3855b1e15abbc91b116390
jcalebbush <jcalebbush@yahoo.com>
2015-01-24 00:01:23 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -9,9 +9,8 @@ unzip("data.zip", files = NULL, list = FALSE, overwrite = TRUE,
 dateDownloaded <- date()
 dateDownloaded
 
-#HERE WE READ THE RELEVANT FILES FOR THE CLEAN UP PROCESS
-
-#HERE IS where we read in THE TEST DATA
+#HERE WE BEGIN TO READ THE RELEVANT FILES FOR THE CLEAN UP PROCESS
+#And THIS IS where we read in THE TEST DATA
 sub_test <-read.table("./UCI HAR Dataset/test/subject_test.txt")
 x_test <- read.table("./UCI HAR Dataset/test/X_test.txt")
 y_test <- read.table("./UCI HAR Dataset/test/y_test.txt")
@@ -20,6 +19,7 @@ y_test <- read.table("./UCI HAR Dataset/test/y_test.txt")
 sub_train <-read.table("./UCI HAR Dataset/train/subject_train.txt")
 x_train <- read.table("./UCI HAR Dataset/train/X_train.txt")
 y_train <- read.table("./UCI HAR Dataset/train/y_train.txt")
+
 
 #HERE IS THE ACTIVITY FILE THAT CONTAINS THE TYPES OF MOVEMENTS THE SUBJECT PERFORMED
 activity <- read.table("./UCI HAR Dataset/activity_labels.txt")
@@ -30,11 +30,13 @@ feat <- read.table("./UCI HAR Dataset/features.txt")
 #HERE WE MERGE THE X TEST AND TRAIN DATA (PART 1 OF THE PROGRAMMING ASSIGNMENT)
 x_test_train <- rbind(x_test, x_train)
 head(x_test_train,2)
+ncol(x_test_train)
 
 
 #HERE WE ASSIGN COLUMN NAMES FROM THE FEATURE.TXT FILE TO THE X TEST AND TRAIN DATA
 names(x_test_train) <- feat[,2]
 head(x_test_train,2)
+names(x_test_train)
 
 #HERE WE ASSIGN COLUMN NAMES TO THE ACTIVITY VALUE FILES 
 names(y_test) <- 'activity_num'
@@ -44,11 +46,15 @@ names(sub_train) <- 'subject'
 
 #HERE WE COMBINE THE SUBJECT DATA AND THE ACTIVITY VALUES FROM THE Y DATA SETS TO THE X TEST AND TRAIN DATA TO CONSOLIDATE THE FILES
 sub_act_x <- cbind(rbind(sub_test, sub_train), rbind(y_test, y_train), x_test_train)
-head(sub_act_x)
+head(sub_act_x, 2)
+ncol(sub_act_x)
+names(sub_act_x)
 
 #HERE WE OBTAIN ONLY THE MEAN AND STD VARIABLES FROM THE X TEST AND TRAIN DATA FRAMES (PART 2 OF THE PROGRAMMING ASSIGNMENT)
-sub_act_x <- sub_act_x[, grepl("mean\\()", names(sub_act_x)) | grepl("std\\()", names(sub_act_x)) | grepl("subject", names(sub_act_x)) | grepl("activity_num",names(sub_act_x))]
+sub_act_x <- sub_act_x[, grepl("mean\\(\\)", names(sub_act_x)) | grepl("std\\(\\)", names(sub_act_x))] #| grepl("subject", names(sub_act_x)) | grepl("activity_num",names(sub_act_x))]
 head(sub_act_x,2)
+ncol(sub_act_x)
+names(sub_act_x)
 
 #HERE WE ASSIGN THE COLUMN NAMES TO THE ACTIVITY NAME FILE (PART 3 OF PROGRAMMING ASSIGNMENT)
 names(activity) <- c('activity_num', 'activity_name')
@@ -57,6 +63,7 @@ merged_data <- merge(activity, sub_act_x, by.x ="activity_num", by.y = "activity
 head(merged_data,2)
 
 #HERE WE CLEAN UP THE COLUMN NAMES REMOVING PARENTHESES AND REPLACING DASHES WITH UNDERSCORES (PART 4 OF PROGRAMMING ASSIGNMENT)
+#We now have a clean and tidy data frame after many transformations.
 column_names <- names(merged_data)
 pattern <- "-"
 column_names <-gsub(pattern,"_", column_names)
@@ -72,3 +79,10 @@ head(merged_data,2)
 sub_act_mean <-aggregate(x=merged_data[,4:69], by=list(Subject = merged_data$subject, Activity = merged_data$activity_name), FUN=mean)
 nrow(sub_act_mean)
 head(sub_act_mean, 2)
+column_names <- names(sub_act_mean)
+column_names <- gsub("tBody", "AVG_tBody", column_names)
+column_names <- gsub("fBody", "AVG_fBody", column_names)
+column_names <- gsub("tGravity", "AVG_tGravity", column_names)
+column_names
+
+write.table(sub_act_mean, file = "mean_by_subject_activity.txt", sep = ",", row.name = FALSE)
